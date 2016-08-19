ec09889c29414b8390907600b9f6d95823a6b4d6
Kangning Song <sunskn@163.com>
2016-08-19 23:48:25 +0800

    clean

--- src/caffe/layers/dyn_conv_layer.cpp
+++ src/caffe/layers/dyn_conv_layer.cpp
@@ -225,19 +225,19 @@ void DynamicConvolutionLayer<Dtype>::Reshape(const vector<Blob<Dtype>*>& bottom,
 }
 template <typename Dtype>
 void DynamicConvolutionLayer<Dtype>::compute_output_shape() {
-  const int* kernel_shape_data = this->kernel_shape_.cpu_data();
-  const int* stride_data = this->stride_.cpu_data();
-  const int* pad_data = this->pad_.cpu_data();
-  const int* dilation_data = this->dilation_.cpu_data();
-  this->output_shape_.clear();
-  for (int i = 0; i < this->num_spatial_axes_; ++i) {
-    // i + 1 to skip channel axis
-    const int input_dim = this->input_shape(i + 1);
-    const int kernel_extent = dilation_data[i] * (kernel_shape_data[i] - 1) + 1;
-    const int output_dim = (input_dim + 2 * pad_data[i] - kernel_extent)
-        / stride_data[i] + 1;
-    this->output_shape_.push_back(output_dim);
-  }
+	const int* kernel_shape_data = this->kernel_shape_.cpu_data();
+	const int* stride_data = this->stride_.cpu_data();
+	const int* pad_data = this->pad_.cpu_data();
+	const int* dilation_data = this->dilation_.cpu_data();
+	this->output_shape_.clear();
+	for (int i = 0; i < this->num_spatial_axes_; ++i) {
+		// i + 1 to skip channel axis
+		const int input_dim = this->input_shape(i + 1);
+		const int kernel_extent = dilation_data[i] * (kernel_shape_data[i] - 1) + 1;
+		const int output_dim = (input_dim + 2 * pad_data[i] - kernel_extent)
+			/ stride_data[i] + 1;
+		this->output_shape_.push_back(output_dim);
+	}
 }
 
 template <typename Dtype>
@@ -250,7 +250,7 @@ void DynamicConvolutionLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bot
 	Dtype* top_data = top[0]->mutable_cpu_data();
 	for (int n = 0; n < this->num_; ++n) {
 		this->forward_cpu_gemm(bottom_data + n * this->bottom_dim_, weight + n * weight_size,
-            top_data + n * this->top_dim_);
+        		 top_data + n * this->top_dim_);
 		if (this->bias_term_) {
 			const Dtype* bias = bottom[2]->cpu_data();
 			this->forward_cpu_bias(top_data + n * this->top_dim_, bias + n * this->num_output_);
@@ -276,7 +276,7 @@ void DynamicConvolutionLayer<Dtype>::Backward_cpu(const vector<Blob<Dtype>*>& to
 	if (this->bias_term_ && this->param_propagate_down_[1]) {
 		caffe_set(bottom[2]->count(), Dtype(0),
 			bottom[2]->mutable_cpu_diff());
-    }
+	}
 	
 	// Bias gradient, if necessary.
 	if (this->bias_term_ && this->param_propagate_down_[1]) {
@@ -290,12 +290,12 @@ void DynamicConvolutionLayer<Dtype>::Backward_cpu(const vector<Blob<Dtype>*>& to
 			// gradient w.r.t. weight. Note that we will accumulate diffs.
 			if (this->param_propagate_down_[0]) {
 				this->weight_cpu_gemm(bottom_data + n * this->bottom_dim_,
-                    top_diff + n * this->top_dim_, weight_diff + n * weight_size);
+                    			top_diff + n * this->top_dim_, weight_diff + n * weight_size);
 			}
 			// gradient w.r.t. bottom data, if necessary.
 			if (propagate_down[0]) {
 				this->backward_cpu_gemm(top_diff + n * this->top_dim_, weight + n * weight_size,
-				    bottom_diff + n * this->bottom_dim_);
+					bottom_diff + n * this->bottom_dim_);
 			}
 		}
 	}
